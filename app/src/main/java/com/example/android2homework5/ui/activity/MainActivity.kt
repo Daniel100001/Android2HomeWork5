16edf9dfafbe48cf1a3af221d2239be1d012cbb6
Daniel100001 <danldplayer2gmail.com>
2023-04-26 15:54:47 +0600

    It's Android 2 Home Work5

---
+++ app/src/main/java/com/example/android2homework5/ui/activity/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.android2homework5.ui.activity
+
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.android2homework5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val permissionMedia = "image/*"
+    private val pickImage = 100
+    private val check = false
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){
+        binding.imageView.setImageURI(it)
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpPermission()
+    }
+
+    private fun setUpPermission() {
+        binding.galleryButton.setOnClickListener {
+            when {
+                checkSelfPermission(READ_EXTERNAL_STORAGE) == PERMISSION_GRANTED -> {
+                    getContent.launch(permissionMedia)
+                }
+                shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE) -> {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+
+                }
+                else -> {
+                    requestPermissions(
+                        arrayOf(READ_EXTERNAL_STORAGE),
+                        pickImage
+                    )
+                }
+            }
+        }
+    }
+
+}
+
