0745355b1dadce2ae552e847b9d559bab904e7a2
Daniel100001 <danldplayer2gmail.com>
2023-05-04 15:13:42 +0600

    It's a Android2 Home Work5

--- app/src/main/java/com/example/android2homework5/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/android2homework5/ui/activity/MainActivity.kt
@@ -2,23 +2,33 @@ package com.example.android2homework5.ui.activity
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.Intent
-import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.android2homework5.databinding.ActivityMainBinding
+import com.example.android2homework5.extensions.hasPermissionCheckAndRequest
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val permissionMedia = "image/*"
-    private val pickImage = 100
+    private var uri: Uri? = null
 
-    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){
-        binding.imageView.setImageURI(it)
-
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { isGranted ->
+        for (permission in isGranted) {
+            when {
+                permission.value -> fileChooserContract.launch("image/*")
+                !shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE) -> {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,26 +41,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUpPermission() {
         binding.galleryButton.setOnClickListener {
-            when {
-                checkSelfPermission(READ_EXTERNAL_STORAGE) == PERMISSION_GRANTED -> {
-                    getContent.launch(permissionMedia)
-                }
-                shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE) -> {
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = Uri.fromParts("package", packageName, null)
-                    intent.data = uri
-                    startActivity(intent)
-
-                }
-                else -> {
-                    requestPermissions(
-                        arrayOf(READ_EXTERNAL_STORAGE),
-                        pickImage
-                    )
-                }
+            if (hasPermissionCheckAndRequest(
+                    requestPermissionLauncher,
+                    arrayOf(READ_EXTERNAL_STORAGE)
+                )
+            ) {
+                fileChooserContract.launch("image/*")
             }
         }
     }
-
+    private val fileChooserContract =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri ->
+            if (imageUri != null) {
+                binding.imageView.setImageURI(imageUri)
+                uri = imageUri
+            }
+        }
 }
+
 
